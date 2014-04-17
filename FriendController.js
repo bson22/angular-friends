e72e206166d70444e6a76fcea484c960cdcae801
Brian Son <bson22@gmail.com>
2014-04-16 22:41:07 -0600

    adds ng-option

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,25 @@
+(function() {
+	angular.module('angular-friends').controller('FriendController', function ($scope, $http) {
+		$scope.list
+		$scope.test = "hello world";
+		$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json").success(function(data){
+			$scope.friends = data.results;
+		});
+		//$scope.searchTerm = $filter('orderBy')(name,'friends')
+		
+		// jQuery.getJSON('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json', function(data) {
+		// 	$scope.$apply(function() {
+		// 		$scope.friends = data.results;
+		// 	});
+		// });
+	$scope.sortArray = [ {string:"Name", val:"name" }, 
+	{string:"Friends", val:"friend_count"}, 
+	{string:"City", val:"current_location.city"}, 
+	{string:"State", val:"current_location.state"},
+	{string:"Country", val:"current_location.country"}
+	];
 
-
-
+	});
+})();
 
 
---
+++ app.js
@@ -0,0 +1,7 @@
+(function() {
+	'use strict';
+
+	var app = angular.module("angular-friends", []);  //creates a var app using the <html ng-app="angular-friends"></html>
+
+})();
+	
